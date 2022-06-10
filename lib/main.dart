963c53dbe631eb1b7576f9c7e6a17d5d2ae1f8e2
Syed Mujtaba Nazki <developer.imujtaba@gmail.com>
2022-06-10 06:50:13 +0530

    Fix lint errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:im_animations/src/color_sonar.dart';
 
 import 'im_animations.dart';
+import 'src/color_sonar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,12 +18,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Homepage(),
+      home: const Homepage(),
     );
   }
 }
 
 class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +38,7 @@ class Homepage extends StatelessWidget {
           // waveMotionEffect: Curves.elasticIn,
           waveMotion: WaveMotion.synced,
           // duration: Duration(seconds: 5),
-          child: CircleAvatar(
+          child: const CircleAvatar(
             radius: 48.0,
             backgroundImage: AssetImage('assets/avatars/man.png'),
           ),
